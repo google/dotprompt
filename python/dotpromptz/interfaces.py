a35bae4c14d5270513d925832030f4a98164db52
r8zr <zephyr.faq@gmail.com>
2025-02-20 15:38:34 +0000

    feat: Port types to Python  ISSUE: #35  CHANGELOG: - [ ] Definition of dataclasses and function interfaces

--- python/dotpromptz/interfaces.py
+++ python/dotpromptz/interfaces.py
@@ -1,26 +1,27 @@
 # Copyright 2025 Google LLC
 # SPDX-License-Identifier: Apache-2.0
 
-"""Type definition."""
+"""Data models and interfaces definition."""
 
 from dataclasses import dataclass, field
-from typing import Any, Generic, Literal, Protocol, TypeAlias, TypeVar
-
+from typing import Any, Generic, Literal, Protocol, TypeVar
 
 T = TypeVar('T')
 
 
-Schema: TypeAlias = dict[str, Any]
+type Schema = dict[str, Any]
 
 
 @dataclass
 class ToolDefinition:
     name: str
-    description: str | None 
+    description: str | None
     input_schema: Schema = field(default_factory=dict)
     output_schema: Schema | None = None
 
-ToolArgument: TypeAlias =  str | ToolDefinition
+
+type ToolArgument = str | ToolDefinition
+
 
 @dataclass
 class HasMetadata:
@@ -28,38 +29,52 @@ class HasMetadata:
     Whether contains metadata.
 
     Attributes:
-        metadata: Arbitrary metadata to be used by tooling or for informational purposes.
+        metadata: Arbitrary metadata to be used by tooling or for informational
+            purposes.
     """
+
     metadata: dict[str, Any] | None = None
 
-@dataclass
+
+@dataclass(kw_only=True)
 class PromptRef:
     name: str
     variant: str | None = None
     version: str | None = None
 
-@dataclass
+
+@dataclass(kw_only=True)
 class PromptData(PromptRef):
     source: str
+
 
 @dataclass
 class PromptMetadata(HasMetadata, Generic[T]):
     """Promt metdata.
-    
+
     Attributes:
       name: The name of the prompt.
       variant: The variant name for the prompt.
       version: The version of the prompt.
       description: A description of the prompt.
-      model: The name of the model to use for this prompt, e.g. `vertexai/gemini-1.0-pro`.
-      tools: Names of tools (registered separately) to allow use of in this prompt.
+      model: The name of the model to use for this prompt, e.g.
+        `vertexai/gemini-1.0-pro`.
+      tools: Names of tools (registered separately) to allow use of in this
+        prompt.
       tool_defs: Definitions of tools to allow use of in this prompt.
       config: Model configuration. Not all models support all options.
       input: Configuration for input variables.
       output: Defines the expected model output format.
-      raw: This field will contain the raw frontmatter as parsed with no additional processing or substitutions. If your implementation requires custom fields they will be available here.
-      ext: Fields that contain a period will be considered "extension fields" in the frontmatter and will be gathered by namespace. For example, `myext.foo: 123` would be available at `parsedPrompt.ext.myext.foo`. Nested namespaces will be flattened, so `myext.foo.bar: 123` would be available at `parsedPrompt.ext["myext.foo"].bar`.
+      raw: This field will contain the raw frontmatter as parsed with no
+        additional processing or substitutions. If your implementation requires
+        custom fields they will be available here.
+      ext: Fields that contain a period will be considered "extension fields" in
+        the frontmatter and will be gathered by namespace. For example,
+        `myext.foo: 123` would be available at `parsedPrompt.ext.myext.foo`.
+        Nested namespaces will be flattened, so `myext.foo.bar: 123` would be
+        available at `parsedPrompt.ext["myext.foo"].bar`.
     """
+
     name: str | None = None
     variant: str | None = None
     version: str | None = None
@@ -73,48 +88,67 @@ class PromptMetadata(HasMetadata, Generic[T]):
     raw: dict[str, Any] | None = None
     ext: dict[str, dict[str, Any]] | None = None
 
-@dataclass
+
+@dataclass(kw_only=True)
 class ParsedPrompt(PromptMetadata[T]):
     template: str
+
 
 @dataclass
 class EmptyPart(HasMetadata):
     pass
 
-@dataclass
+
+@dataclass(kw_only=True)
 class TextPart(EmptyPart):
     text: str
 
-@dataclass
+
+@dataclass(kw_only=True)
 class DataPart(EmptyPart):
     data: dict[str, Any]
 
-@dataclass
+
+@dataclass(kw_only=True)
 class MediaPart(EmptyPart):
     media: dict[str, str | None]
 
-@dataclass
+
+@dataclass(kw_only=True)
 class ToolRequestPart(EmptyPart, Generic[T]):
     tool_request: dict[str, T | None]
 
-@dataclass
+
+@dataclass(kw_only=True)
 class ToolResponsePart(EmptyPart, Generic[T]):
     tool_response: dict[str, T | None]
 
-@dataclass
+
+@dataclass(kw_only=True)
 class PendingPart(EmptyPart):
-    metadata: dict[str, Any] = field(default_factory=lambda: {"pending": True})
+    metadata: dict[str, Any] = field(default_factory=lambda: {'pending': True})
 
-Part: TypeAlias = TextPart | DataPart | MediaPart | ToolRequestPart[Any] | ToolResponsePart[Any] | PendingPart
 
-@dataclass
+type Part = (
+    TextPart
+    | DataPart
+    | MediaPart
+    | ToolRequestPart[Any]
+    | ToolResponsePart[Any]
+    | PendingPart
+)
+
+
+@dataclass(kw_only=True)
 class Message(HasMetadata):
-    role: Literal["user", "model", "tool", "system"]
+    role: Literal['user', 'model', 'tool', 'system']
     content: list[Part]
 
-@dataclass
+
+@dataclass(kw_only=True)
 class Document(HasMetadata):
     content: list[Part]
+
 
 @dataclass
 class DataArgument(Generic[T]):
@@ -124,100 +158,134 @@ class DataArgument(Generic[T]):
     Attributes:
         input: Input variables for the prompt template.
         docs: Relevant documents.
-        messages: Previous messages in the history of a multi-turn conversation. 
-        context: Items in the context argument are exposed as `@` variables, e.g. `context: {state: {...}}` is exposed as `@state`.
+        messages: Previous messages in the history of a multi-turn conversation.
+        context: Items in the context argument are exposed as `@` variables,
+            e.g. `context: {state: {...}}` is exposed as `@state`.
     """
+
     input: T | None = None
     docs: list[Document] | None = None
     messages: list[Message] | None = None
     context: dict[str, Any] | None = None
 
-JsonSchema: TypeAlias = Any
+
+type JsonSchema = Any
+
 
 class SchemaResolver(Protocol):
-    """Function that can resolve a provided schema name to an underlying JSON schema.
-    
+    """Resolves a provided schema name to an underlying JSON schema.
+
     Utilized for shorthand to a schema library provided by an external tool.
     """
 
-    def __call__(self, schemaName: str) -> JsonSchema | None:
-        ...
+    def __call__(self, schema_name: str) -> JsonSchema | None: ...
+
 
 class ToolResolver(Protocol):
-    """Function that can resolve a provided tool name to an underlying ToolDefinition. 
-    
+    """Resolves a provided tool name to an underlying ToolDefinition.
+
     Utilized for shorthand to a tool registry provided by an external library.
     """
 
-    def __call__(self, toolName: str) -> ToolDefinition | None:
-        ...
+    def __call__(self, tool_name: str) -> ToolDefinition | None: ...
 
-@dataclass
+
+@dataclass(kw_only=True)
 class RenderedPrompt(PromptMetadata[T]):
     """The final result of rendering a Dotprompt template.
 
-    It includes all of the prompt metadata as well as a set of `messages` to be sent to the  model.
+    It includes all of the prompt metadata as well as a set of `messages` to be
+    sent to the  model.
 
     Attributes:
         messages: The rendered messages of the prompt.
     """
-    
+
     messages: list[Message]
 
+
 class PromptFunction(Protocol, Generic[T]):
-    """Function that takes runtime data/context and returns a rendered prompt result.
-    """
+    """Takes runtime data/context and returns a rendered prompt result."""
 
     prompt: ParsedPrompt[T]
 
-    def __call__(self, data: DataArgument[Any, Any], options: PromptMetadata[T] | None = None) -> RenderedPrompt[T]:
-        ...
+    def __call__(
+        self,
+        data: DataArgument[Any],
+        options: PromptMetadata[T] | None = None,
+    ) -> RenderedPrompt[T]: ...
+
 
 class PromptRefFunction(Protocol, Generic[T]):
-    """Function that takes runtime data / context and returns a rendered prompt result after loading a prompt via reference.
+    """Takes runtime data / context and returns a rendered prompt result.
+
+    The difference in comparison to PromptFunction is that a promp is loaded via
+    reference.
     """
 
-    def __call__(self, data: DataArgument[Any, Any], options: PromptMetadata[T] | None = None) -> RenderedPrompt[T]:
-        ...
+    def __call__(
+        self,
+        data: DataArgument[Any],
+        options: PromptMetadata[T] | None = None,
+    ) -> RenderedPrompt[T]: ...
+
     prompt_ref: PromptRef
+
 
 @dataclass
 class PaginatedResponse:
     cursor: str | None = None
 
-@dataclass
+
+@dataclass(kw_only=True)
 class PartialRef:
     name: str
     variant: str | None = None
     version: str | None = None
 
-@dataclass
+
+@dataclass(kw_only=True)
 class PartialData(PartialRef):
     source: str
+
 
 class PromptStore(Protocol):
     """PromptStore is a common interface that provides for."""
 
     def list(self, options: dict[str, Any] | None = None) -> dict[str, Any]:
-        """Return a list of all prompts in the store (optionally paginated). Some store providers may return limited metadata."""
-    
-    def list_partials(self, options: dict[str, Any] | None = None) -> dict[str, Any]:
+        """Return a list of all prompts in the store (optionally paginated).
+
+        Be aware that some store providers may return limited metadata.
+        """
+
+    def list_partials(
+        self, options: dict[str, Any] | None = None
+    ) -> dict[str, Any]:
         """Return a list of partial names available in this store."""
-    
-    def load(self, name: str, options: dict[str, Any] | None = None) -> PromptData:
+
+    def load(
+        self, name: str, options: dict[str, Any] | None = None
+    ) -> PromptData:
         """Retrieve a prompt from the store."""
-    
-    def load_partial(self, name: str, options: dict[str, Any] | None = None) -> PromptData:
+
+    def load_partial(
+        self, name: str, options: dict[str, Any] | None = None
+    ) -> PromptData:
         """Retrieve a partial from the store."""
 
+
 class PromptStoreWritable(PromptStore, Protocol):
-    """PromptStore that also has built-in methods for writing prompts in addition to reading them."""
+    """PromptStore that also has built-in methods for writing prompts."""
 
     def save(self, prompt: PromptData) -> None:
-        """Save a prompt in the store. May be destructive for prompt stores without versioning."""
+        """Save a prompt in the store.
+
+        May be destructive for prompt stores without versioning.
+        """
 
     def delete(self, name: str, options: dict[str, Any] | None = None) -> None:
-        """Delete a prompt from the store. """
+        """Delete a prompt from the store."""
+
 
 @dataclass
 class PromptBundle:
