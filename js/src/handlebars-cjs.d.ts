0ef6629e138e4ed2ca0db4788db05f1a3c084542
Michael Visser <michaelhvisser@gmail.com>
2026-09-23 06:26:39 -0500

    style(js): satisfy Handlebars import check

--- js/src/handlebars-cjs.d.ts
+++ js/src/handlebars-cjs.d.ts
@@ -22,6 +22,7 @@ declare module 'handlebars/dist/cjs/handlebars.js' {
   // Import and re-export everything from the main handlebars package
   // This leverages the official types from node_modules/handlebars/types/index.d.ts
   export * from 'handlebars';
+
   import handlebars from 'handlebars';
   export default handlebars;
 }
